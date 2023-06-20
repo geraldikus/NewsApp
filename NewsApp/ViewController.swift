95f11c6645d95bd0355e6cfee84f021ee2fddd69
Anton Kasian <34170533+AntonKasian@users.noreply.github.com>
2023-06-20 10:13:26 +0400

    Initial commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Anton on 20.06.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
